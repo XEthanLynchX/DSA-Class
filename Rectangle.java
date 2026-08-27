74cc4041e10eb4a45e19647b9d6650ca58591d4f
Ethan Lynch <76799772+XEthanLynchX@users.noreply.github.com>
2026-08-27 17:24:54 -0400

    init commit

---
+++ Rectangle.java
@@ -0,0 +1,31 @@
+// Name: Ethan Lynch
+// Class: CS 3305/Section#3
+// Term: Fall 2026
+// Instructor: Dr. Haddad
+// Assignment: 1
+// IDE Name: Visual Studio Code
+
+public class Rectangle {
+    private double width = 1.0;
+    private double height = 1.0;
+
+    public Rectangle() {
+        width = this.width;
+        height = this.height;
+    }
+
+    public Rectangle(double width, double height){ 
+        width = this.width;
+        height = this.height;
+    }
+
+    public double getHeight(){ 
+        return this.height;
+    }
+
+    public double getWidth(){ 
+        return this.width;
+    }
+
+    
+}
